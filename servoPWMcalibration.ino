9c615248535e4496be8b175c7046e384aaba5cbe
drchimes <chimes@misshalls.org>
2025-01-25 16:59:12 -0500

    Update servoPWMcalibration.ino

--- servoPWMcalibration.ino
+++ servoPWMcalibration.ino
@@ -3,82 +3,50 @@ This code is modifed from:
 Using Serial Monitor to Control Servo Motor (11/14/2018). Kub_Luk
 <https://projecthub.arduino.cc/Kub_Luk/using-serial-monitor-to-control-servo-motor-c55083>
 Accessed on 11/17/2024
+
+the range Dr. Chris determined is 100 - 490. try yours
 */
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+#define SERVO_FREQ 50  // Analog servos run at ~50 Hz updates
 
-#define SERVO_FREQ 50
-/////Update these values as needed to control a motor and its position
-int servonum = 0;
-int posValue = 160;
-
-void setup(){
+void setup() {
   Serial.begin(9600);
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(SERVO_FREQ);
-}
-void loop(){
-  pwm.setPWM(servonum, 0, posValue);
-  message();
-}
-
-void message(){
-  Serial.print("Servo ");
-  Serial.print(servonum);
-  Serial.print(" is at ");
-  Serial.println(posValue);
-  delay(500);
-}
-
-
-
-////////////////////Ignore this code//////////////////////////
-/*
-  while (!Serial);
+  delay(10);
+  while (!Serial)
+    
   Serial.println("-------------------------");
   Serial.println("ARos   is loading....");
   delay(1000);
-  Serial.println("ARos loaded successfully");
+  Serial.println("ARos loaded succesfully");
   Serial.println("-------------------------");
 
-Serial.println("calibrating   servo...");
-for(pos = 1600; pos <= 600; pos += 1)
-pwm.setPWM(0,0,160);//myservo.write(0);
-delay(1000);
-pwm.setPWM(0,0,600);//myservo.write(180);
-delay(1000);
-pwm.setPWM(0,0,380);//myservo.write(90);
-delay(1000);
-Serial.println("servo   calibrated");
-Serial.println("-------------------------");
-Serial.println("Comand   input online, write command to perform action");
-Serial.println("-------------------------");
-
+  Serial.println("calibrating   servo...");
+  pwm.setPWM(1, 0, 100);  ///update
+  delay(1000);
+  pwm.setPWM(1, 0, 490);
+  delay(1000);
+  pwm.setPWM(1, 0, 298);
+  delay(1000);
+  Serial.println("servo   calibrated");
+  Serial.println("-------------------------");
+  Serial.println("Comand   input online, write command to perform action");
+  Serial.println("-------------------------");
 }
 
-void   loop() {
-  
-for(pos = 160; pos <= 600; pos += 1)
-if (Serial.available()){
-   int state = Serial.parseInt();
-  if (state < 165){
-    Serial.print(">");
-    Serial.println(state);
-    Serial.println("cannot execute command, the number is too lowr");
-  }
-  if (state >= 165 && state < 595){
-    Serial.print(">");
-    Serial.println(state);
+
+void loop() {
+  if (Serial.available()) {
+    int state = Serial.parseInt();
+
+    pwm.setPWM(1, 0, state);
+    delay(1000);
     Serial.print("turning servo to ");
-    Serial.print(state);
-    Serial.println(" PWM");
-    pwm.setPWM(0,0,state);//myservo.write(state);
+    Serial.println(state);
   }
 }
-}
-*/
 
-
-  
